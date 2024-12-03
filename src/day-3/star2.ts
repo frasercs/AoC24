be069e14d1f0fb3147736e6fa7bfb9d105442b6d
frasercs <fmacdonald150@gmail.com>
2024-12-03 13:30:44 +0000

    I didn't use an apostrophe so that took far too long... thanks for being a rubber ducky @black-butterflies

--- src/day-3/star2.ts
+++ src/day-3/star2.ts
@@ -1,36 +1,44 @@
-import * as fs from "fs";
+import * as fs from 'fs';
 
-const lines: string[] = [];
-
-fs.readFile(
-  "C:/Users/Administrator/AoC/src/day-3/input.txt", // for at work: "C:/Users/Administrator/AoC/src/day-3/input.txt" // for at home: /home/fraser/AoC24/src/day-3/input.txt
-  "utf8",
-  (err: any, data: string) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    lines.push(data);
-    processLines();
-  }
-);
-
-function processLines() {
+function processFile(filePath: string) {
     let sum: number = 0;
+    let canDo: boolean = true;
     const regexp: RegExp = /mul\((\d{1,3}),(\d{1,3})\)/g;
-  
-    for (const line of lines) {
-      if (line === "") {
-        continue;
-      }
-  
-      let match;
-      while ((match = regexp.exec(line)) !== null) {
-        const num1 = parseInt(match[1]);
-        const num2 = parseInt(match[2]);
-        sum += num1 * num2;
-      }
+
+    // Read the file content
+    const content = fs.readFileSync(filePath, 'utf-8');
+
+    let index = 0;
+
+    while (index < content.length) {
+        // Check for do() and don't() first to set the canDo flag
+        if (content.substring(index, index + 4) === 'do()') {
+            canDo = true;
+            index += 4;
+        } else if (content.substring(index, index + 7) === 'don\'t()') {
+            canDo = false;
+            index += 7;
+        } else {
+            // Only perform multiplication if canDo is true
+            if (canDo) {
+                regexp.lastIndex = index;
+                const match = regexp.exec(content);
+                if (match && match.index === index) {
+                    const num1 = parseInt(match[1]);
+                    const num2 = parseInt(match[2]);
+                    sum += num1 * num2;
+                    index += match[0].length;
+                } else {
+                    index++;
+                }
+            } else {
+                index++;
+            }
+        }
     }
-  
+
     console.log(sum);
-  }
+}
+
+// Call the function with the path to your file
+processFile('src/day-3/input.txt');
